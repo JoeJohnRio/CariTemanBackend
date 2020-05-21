080bf6d0b454aac28d07927c50c677f525908997
JoeJohnRio <joejohnrio@gmail.com>
2020-05-21 13:34:02 +0700

    ordinaryseeder

--- database/seeds/LokasiPklSeeder.php
+++ database/seeds/LokasiPklSeeder.php
@@ -11,9 +11,30 @@ class LokasiPklSeeder extends Seeder
      */
     public function run()
     {
+        $kota = array("Banda Aceh", 
+        "Langsa", 
+        "Bengkulu", 
+        "Sungai Penuh", 
+        "Cimahi", 
+        "Tasikmalaya", 
+        "Semarang", 
+        "Kota Administrasi Jakarta Utara", 
+        "Kota Administrasi Jakarta Timur", 
+        "Bogor", 
+        "Tasikmalaya", 
+        "Pekalongan", 
+        "Purwokerto", 
+        "Kediri", 
+        "Singkawang", 
+        "Tarakan", 
+        "Batam", 
+        "Tanjungpinang", 
+        "Parepare" 
+    );
+
         for($i = 1; $i <= 50; $i++){
     		DB::table('lokasi_pkl')->insert([
-    			'nama_kota' => "kota $i",
+    			'nama_kota' => $kota[mt_rand(0,18)],
     		]);
     	}
     }
--- database/seeds/TempatPklSeeder.php
+++ database/seeds/TempatPklSeeder.php
@@ -12,6 +12,27 @@ class TempatPklSeeder extends Seeder
      */
     public function run()
     {
+        $perusahaan = array("PT. Kuala Pelabuhan Indonesia", 
+        "PT. Kurnia Jaya Raya", 
+        "PT. Kyung Dong Indonesia", 
+        "PT. Kyungseung Trading Indonesia", 
+        "PT. Ladangrumput Suburabadi", 
+        "PT. Les Nouveaux Premier Real Property Indonesia", 
+        "PT. Maskapai Perkebunan Leidong West Indonesia", 
+        "PT. Marga Nusantara Jaya", 
+        "PT. Marta Unikatama", 
+        "PT. McDermott Indonesia", 
+        "PT. Meares Soputan Mining", 
+        "PT. Mega Kemiraya", 
+        "PT. Melputra Garmindo", 
+        "PT. Mesitechmitra Purnabangun", 
+        "PT. Metec Semarang", 
+        "PT. Mintek Dendrill Indonesia", 
+        "PT. Mitra Infoparama", 
+        "PT. Mitraland Harapan Sejati", 
+        "PT. Molten Aluminium Producer Indonesia" 
+    );
+
         $url_1 = "https://strategimanajemen.net/apps23/wp-content/uploads/2018/04/re-apt.jpg";
         $url_2 = "https://www.jagoteknologi.com/wp-content/uploads/2018/05/Alphabet-Google.jpg";
         $url_3 = "https://cdn.idntimes.com/content-images/community/2019/06/1-504c3148a79864e3781dc70ec656da3f_600x400.jpg";
@@ -21,7 +42,7 @@ class TempatPklSeeder extends Seeder
         $gambar_url = array( $url_1, $url_2, $url_3, $url_4, $url_5, $url_6 );
         for($i = 1; $i <= 50; $i++){
     		DB::table('tempat_pkl')->insert([
-    			'nama_perusahaan' => "PT. Tempat Pkl $i",
+    			'nama_perusahaan' => $perusahaan[mt_rand(0,18)],
                 'id_lokasi_pkl' => mt_rand(1,50),
                 'phone_number'=> 55500000+mt_rand(00000, 99999),
                 'gambar' => $gambar_url[mt_rand(0,5)],
