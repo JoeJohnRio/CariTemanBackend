db8739ca2cc5fe18cddffe6914da7ef5022f7648
JoeJohnRio <joejohnrio@gmail.com>
2020-05-21 13:53:10 +0700

    ordinary

--- app/RelationTempatPkl.php
+++ app/RelationTempatPkl.php
@@ -10,6 +10,10 @@ class RelationTempatPkl extends Model
 
     protected $fillable = ['is_favorite', 'id_mahasiswa', 'id_tempat_pkl'];
 
+    public function tempat_pkl(){	
+        return $this->belongsTo('App\TempatPkl', 'id_tempat_pkl');	
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
