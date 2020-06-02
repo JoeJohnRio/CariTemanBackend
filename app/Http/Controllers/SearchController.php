78304685d0879de894dedfa94e821e3a956c2ed2
JoeJohnRio <joejohnrio@gmail.com>
2020-06-03 06:00:32 +0700

    ordinary

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\LokasiPkl;
 use Illuminate\Http\Request;
 use App\Mahasiswa;
 use App\RelationBidangKerja;
 use App\TempatPkl;
 use App\RelationSkillhobi;
 use App\SearchHistory;
+use App\BidangKerja;
+use App\RelationLokasiPkl;
+use App\UlasanTempatPkl;
+use App\RelationTempatPkl;
+use \stdClass;
 
 class SearchController extends Controller
 {
@@ -48,7 +54,7 @@ class SearchController extends Controller
         }
         
         $keyword = $request->keyword;
-            $user = Mahasiswa::with('pengalaman_lomba', 
+            $user = Mahasiswa::with('pengalaman_lomba.relation_bidang_kerja.bidang_kerja', 
             'pengalaman_organisasi.relation_bidang_kerja.bidang_kerja',
             'relation_teman')->
             where('is_verified',1)->where('preferensi', $preferensi)->where('name', 'like', "%".$keyword."%");    
@@ -57,19 +63,19 @@ class SearchController extends Controller
             $user->where('jenis_kelamin', $request->input('jenis_kelamin'));
         }
 
-        if ($request->id_fakultas != null) {
+        if ($request->id_fakultas != null && $request->id_fakultas != 0) {
             $user->where('id_fakultas', $request->input('id_fakultas'));
         }
 
-        if ($request->id_program_studi != null) {
+        if ($request->id_program_studi != null && $request->id_program_studi != 0) {
             $user->where('id_program_studi', $request->input('id_program_studi'));
         }
 
-        if ($request->id_keminatan != null){
+        if ($request->id_keminatan != null && $request->id_keminatan != 0){
             $user->where('id_keminatan', $request->input('id_keminatan'));
         }
 
-        if ($request->tahun_mulai != null) {
+        if ($request->tahun_mulai != null && $request->tahun_mulai != 2012) {
             $user->where('tahun_mulai', $request->input('tahun_mulai'));
         }
 
@@ -93,7 +99,7 @@ class SearchController extends Controller
 
         public function searchTempatPkl(request $request){
             $keyword = $request->keyword;
-            $tempat_pkl = TempatPkl::where('nama_perusahaan', 'like', "%".$keyword."%");    
+            $tempat_pkls = TempatPkl::where('nama_perusahaan', 'like', "%".$keyword."%");    
                 
             $all = collect();
 
@@ -104,26 +110,50 @@ class SearchController extends Controller
             $searchHistory->save();
                 
             if ($request->id_lokasi_pkl != null) {
-                $tempat_pkl = $tempat_pkl->where('id_lokasi_pkl', $request->input('id_lokasi_pkl'));
+                $tempat_pkls = $tempat_pkls->where('id_lokasi_pkl', $request->input('id_lokasi_pkl'));
             }
 
-            if ($request->id_relation_bidang_kerja != null) {
-                foreach ($tempat_pkl->get() as $one_tempat_pkl) {
-                    if(RelationBidangKerja::where('id_tempat_pkl', $one_tempat_pkl->id)->where('id_bidang_kerja',
-                    $request->input('id_relation_bidang_kerja'))->first() != null)
+            if ($request->id_bidang_kerja != null) {
+                foreach ($tempat_pkls->get() as $tempat_pkl) {
+                    if(RelationBidangKerja::where('id_tempat_pkl', $tempat_pkl->id)->where('id_bidang_kerja',
+                    $request->id_bidang_kerja)->first() != null)
                     {
-                        $all->add($one_tempat_pkl);
+                        $all->add($tempat_pkl);
                     }
                 }
-                return response()->json(
-                    ['tempat_pkl' => $all]
-                );
+            }else{
+                $all = $tempat_pkls->get();
             }
 
-            return response()->json(
-                ['tempat_pkl' => $tempat_pkl->get()]
-            );
+            $semua = collect();
 
+            $tempatPkls = $all;
+            foreach($tempatPkls as $tempatPkl){
+                $returnObject = new stdClass();
+                $returnObject->id = $tempatPkl->id;
+                $returnObject->gambar = $tempatPkl->gambar;
+                $checkIsFavorite = RelationTempatPkl::where('id_mahasiswa', auth()->user()->id)->where('id_tempat_pkl', $tempatPkl->id)->first();
+                    if($checkIsFavorite != null){
+                        $returnObject->is_favorite = $checkIsFavorite->is_favorite;
+                    }else{
+                        $returnObject->is_favorite = 0;
+                    }
+                $returnObject->type_of_recommendation = $request->type_of_recommendation;
+                $returnObject->nama_perusahaan = $tempatPkl->nama_perusahaan;
+                $returnObject->jumlah_rekomendasi = UlasanTempatPkl::where('id_tempat_pkl', $tempatPkl->id)->count();
+                $returnObject->id_bidang_kerja = RelationBidangKerja::where('id_tempat_pkl', $tempatPkl->id)->first()->id;
+                $returnObject->nama_bidang_kerja = BidangKerja::find(RelationBidangKerja::where('id_tempat_pkl', $tempatPkl->id)->first()->id_bidang_kerja)->nama_bidang_kerja;
+                $returnObject->nama_kota = LokasiPkl::find(RelationLokasiPkl::where('id_tempat_pkl', $tempatPkl->id)->first()->id_lokasi_pkl)->nama_kota;
+                $semua->add($returnObject);
+            }
+
+            
+            $res = [];
+            $test = $semua->sortByDesc('jumlah_rekomendasi');
+            foreach ($test  as $key => $value) {
+                $res[] = $value;
+            }
+            return $res;
             }
     }
 
